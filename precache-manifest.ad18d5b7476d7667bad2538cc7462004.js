151219cf448877d8550d04b14f5c6e4dc094ee1c
Zicozydasilva <Zicozydasilva70@gmail.com>
2020-06-25 22:51:14 +0100

    Updates

---
+++ precache-manifest.ad18d5b7476d7667bad2538cc7462004.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b690c88e8fe0e5788bc4091d27b387",
+    "url": "/My-Recipe/index.html"
+  },
+  {
+    "revision": "5d02d4b9abd43990bd81",
+    "url": "/My-Recipe/static/css/main.20e09760.chunk.css"
+  },
+  {
+    "revision": "75ffd7e2b60e5c98299a",
+    "url": "/My-Recipe/static/js/2.aee74326.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-Recipe/static/js/2.aee74326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d02d4b9abd43990bd81",
+    "url": "/My-Recipe/static/js/main.730f1908.chunk.js"
+  },
+  {
+    "revision": "bf9c3eb82c5c933484a0",
+    "url": "/My-Recipe/static/js/runtime-main.c296673b.js"
+  }
+]);
